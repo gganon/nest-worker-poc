9203c72c0dd73febc4cfa05b717afe8b8d3c1cc1
GGanon <77290884+gganon@users.noreply.github.com>
2022-09-13 07:22:50 +0530

    Add long running synchronous compuation

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Header, HttpCode, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { pbkdf2Sync, randomBytes } from 'crypto';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,13 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('/strong-hash')
+  @HttpCode(200)
+  @Header('Content-Type', 'text/plain')
+  getStrongHash(@Body('input') input: string) {
+    const buffer = pbkdf2Sync(input, randomBytes(64), 5000000, 64, 'sha512');
+    return buffer.toString('base64');
   }
 }
